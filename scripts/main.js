9fe8b999975dd6da0d2b6752bb54e4f3ac5ec8f5
anneliesetaylor <anneliesetaylor@users.noreply.github.com>
2020-11-13 18:08:06 -0500

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,65 @@
+
+/* DOM EVENT #1 */
+
+let image = document.querySelector('#coffee_pic');
+
+let hot = 'images/hot_coffee.jpg';
+let iced = 'images/iced_coffee.jpg';
+
+image.onmouseenter = (e) => {
+	// change image
+   e.target.setAttribute('src', iced);
+};
+
+image.onmouseleave = (e) => {
+// change image
+   e.target.setAttribute('src', hot);
+};
+
+
+/* DOM EVENT #2 */
+
+const clicker = document.querySelector('#latte');
+const text = document.querySelector('#latteinfo');
+// hide the image
+text.style.display = 'none';
+
+clicker.onclick = () => {
+  if (text.style.display === 'none') {
+    text.style.display = 'block';
+  } else if (text.style.display === 'block') {
+       text.style.display = 'none';
+};
+};
+
+
+
+
+
+const clicker_two = document.querySelector('#cappuccino');
+const text_two = document.querySelector('#cappuccinoinfo');
+// hide the image
+text_two.style.display = 'none';
+
+clicker_two.onclick = () => {
+  if (text_two.style.display === 'none') {
+    text_two.style.display = 'block';
+  } else if (text_two.style.display === 'block') {
+       text_two.style.display = 'none';
+};
+};
+
+
+
+const clicker_three = document.querySelector('#macchiato');
+const text_three = document.querySelector('#macchiatoinfo');
+// hide the image
+text_three.style.display = 'none';
+
+clicker_three.onclick = () => {
+  if (text_three.style.display === 'none') {
+    text_three.style.display = 'block';
+  } else if (text_three.style.display === 'block') {
+       text_three.style.display = 'none';
+};
+};
